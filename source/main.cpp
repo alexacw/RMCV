f5f82f9e756666e1c1962b2a33985f8ed188dab8
alexacw <alex_acw@outlook.com>
2017-10-22 13:49:36 +0800

    added more tuning interfaces

--- source/main.cpp
+++ source/main.cpp
@@ -12,12 +12,23 @@ int main(int argc, char **argv)
         return 0;
 
     //windows initializations
-    //adaptive threshold tuning
+    //window- Gaussian Blur
+    namedWindow(Gau_blur_window_name, CV_WINDOW_AUTOSIZE);
+    createTrackbar(Gau_blur_size_tackbar, Gau_blur_window_name, &Gau_blur_size, Gau_blur_max_size);
+
+    //window-adaptive threshold tuning
     namedWindow(adpt_ts_window_name, CV_WINDOW_AUTOSIZE);
     createTrackbar(adpt_ts_trackbar_adaptiveMethod, adpt_ts_window_name, &adpt_ts_adaptiveMethod, adpt_ts_max_adaptiveMethod);
     createTrackbar(adpt_ts_trackbar_thresholdType, adpt_ts_window_name, &adpt_ts_thresholdType, adpt_ts_max_thresholdType);
     createTrackbar(adpt_ts_trackbar_blockSize, adpt_ts_window_name, &adpt_ts_blockSize, adpt_ts_max_blockSize);
     createTrackbar(adpt_ts_trackbar_subConstant, adpt_ts_window_name, &adpt_ts_subConstant, adpt_ts_max_subConstant);
+
+    //window-morphologytuning
+    namedWindow(mplg_window_name, CV_WINDOW_AUTOSIZE);
+    createTrackbar(mplg_trackbar_shape, mplg_window_name, &mplg_shape, mplg_max_shape);
+    createTrackbar(mplg_trackbar_witdth, mplg_window_name, &mplg_size_witdth, mplg_max_size_witdth);
+    createTrackbar(mplg_trackbar_height, mplg_window_name, &mplg_size_height, mplg_max_size_height);
+    createTrackbar(mplg_trackbar_opertaion, mplg_window_name, &mplg_opertaion, mplg_max_opertaion);
 
     while (1)
     {
@@ -26,18 +37,22 @@ int main(int argc, char **argv)
         if (frame.empty())
             break; // end of video stream
 
-        //namedWindow(adpt_ts_window_name, CV_WINDOW_AUTOSIZE);
+        //convert ot B&W
         cvtColor(frame, grayA, CV_BGR2GRAY);
-        //imshow("gray", grayA);
 
+        //Blur
         GaussianBlur(grayA, gauA, Size(17, 17), 0);
-        imshow("blur", grayA);
-
+        imshow(Gau_blur_window_name, gauA);
+        
+        //highlight foreground
         adaptiveThreshold(gauA, binaryA, adpt_ts_BINARY_value, adpt_ts_adaptiveMethod, adpt_ts_thresholdType, ((adpt_ts_blockSize * 2 + 1) > 1) ? (adpt_ts_blockSize * 2 + 1) : 3, adpt_ts_subConstant);
         imshow(adpt_ts_window_name, binaryA);
 
-        //morphologyEx(binaryA, morphoA, MORPH_OPEN, Mat());
-        //imshow("morph errode ", binaryA);
+        //make segment clear by morpho
+        Mat morphoKernel = getStructuringElement(MORPH_ELLIPSE, Size(1, 5));
+
+        morphologyEx(binaryA, morphoA, MORPH_OPEN, morphoKernel);
+        imshow(mplg_window_name, morphoA);
 
         if (waitKey(1) == 27)
             break; // stop capturing by pressing ESC
--- source/main.h
+++ source/main.h
@@ -31,22 +31,54 @@ bool number_segment[10][7] = {
     {1, 1, 1, 1, 1, 1, 1},
     {1, 1, 1, 1, 0, 1, 1}};
 
+//Gaussian Blur values
+int Gau_blur_size = 8;
+
+int const Gau_blur_max_size = 50;
+
+char *const Gau_blur_window_name = "Gaussian Blur";
+
+char *const Gau_blur_size_tackbar = "Gaussian Blur Size (2n+1)";
+
 //adaptive threshold values
 int const adpt_ts_BINARY_value = 255;
 
-int adpt_ts_adaptiveMethod = 0;
-int adpt_ts_thresholdType = 0;
+int adpt_ts_adaptiveMethod = 1;
+int adpt_ts_thresholdType = 1;
 int adpt_ts_blockSize = 1; //here blocksize means half length of diagonal
 int adpt_ts_subConstant = 0;
 
-int adpt_ts_max_adaptiveMethod = 1;
-int adpt_ts_max_thresholdType = 1;
-int adpt_ts_max_blockSize = 50; //change if necessary, sampling size to neigherhoods to determine threshold, calculation needed as the blocksize must be 1+2n where n is a positive integer
-int adpt_ts_max_subConstant = 255;
+int const adpt_ts_max_adaptiveMethod = 1;
+int const adpt_ts_max_thresholdType = 1;
+int const adpt_ts_max_blockSize = 50; //change if necessary, sampling size to neigherhoods to determine threshold, calculation needed as the blocksize must be 1+2n where n is a positive integer
+int const adpt_ts_max_subConstant = 255;
 
-char *adpt_ts_window_name = "Tune adaptive threshold filter";
+char *const adpt_ts_window_name = "Tune adaptive threshold filter";
 
-char *adpt_ts_trackbar_adaptiveMethod = "adaptiveMethod: \n 0: Mean \n 1: Gaussian";
-char *adpt_ts_trackbar_thresholdType = "thresholdType: \n 0: Binary \n 1: Binary Inverse";
-char *adpt_ts_trackbar_blockSize = "blockSize:";
-char *adpt_ts_trackbar_subConstant = "Subtraction Constant:";
+char *const adpt_ts_trackbar_adaptiveMethod = "adaptiveMethod: \n 0: Mean \n 1: Gaussian";
+char *const adpt_ts_trackbar_thresholdType = "thresholdType: \n 0: Binary \n 1: Binary Inverse";
+char *const adpt_ts_trackbar_blockSize = "blockSize:";
+char *const adpt_ts_trackbar_subConstant = "Subtraction Constant:";
+
+//Morphological
+char *const mplg_window_name = "Morphological transform values";
+
+//Morphological-kernel
+int mplg_shape = 0;
+int mplg_size_witdth = 1; //these size will be distance from center to edge
+int mplg_size_height = 1;
+
+int const mplg_max_shape = 2;        // rect, cross, ellipse
+int const mplg_max_size_witdth = 50; //these size will be distance from center to edge
+int const mplg_max_size_height = 50;
+
+char *const mplg_trackbar_shape = "kernel shape:\n 0:rectangle\n1:cross\n2:ellipse";
+char *const mplg_trackbar_witdth = "kernel width (= value*2 + 1)";
+char *const mplg_trackbar_height = "kernel height (= value*2 + 1)";
+
+//Morphological-values
+int mplg_opertaion = 0;
+
+int const mplg_max_opertaion = 6;
+
+char *const mplg_trackbar_opertaion = "0: Erode \n1: Dilate \n2: Open \n3: Close \n4: Gradient \n5: Tophat \n6: Blackhat";
