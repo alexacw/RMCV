075aeaeb271a0d819de3e213a7b52f5ff909910b
Alex Au <alex_acw@outlook.com>
2017-10-25 00:40:19 +0800

    modified the min red value to adapt to different ambient brightness

--- source/main.h
+++ source/main.h
@@ -47,7 +47,7 @@ const float frameShiftMultiplier = 0.15;
 const float cropPercent = 0.0;
 
 //Color Range Threshol Vlaues
-int low_r = 190,
+int low_r = 150,
     high_r = 255,
     low_g = 0,
     high_g = 255,
